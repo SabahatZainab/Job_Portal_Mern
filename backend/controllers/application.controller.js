a1997473bc5485484ae91d6b4500049d15f0ac2c
Sabahat Zainab <sabahatzainab65@gmail.com>
2024-10-01 15:39:56 +0500

    worked on application logic

--- backend/controllers/application.controller.js
+++ backend/controllers/application.controller.js
@@ -43,6 +43,97 @@ export const applyJob = async (req,res) =>{
         })
 
     } catch (error) {
-        
+        console.log(error)
+    }
+}
+
+export const getAppliedJobs = async (req,res) => {
+    try {
+        const userId = req.id;
+        const application = await Application.find({applicant:userId}).sort({createdAt:-1}).populate({
+            path:'job',
+            options:{sort:{createdAt:-1}},
+            populate:{
+                path:'company',
+                options:{sort:{createdAt:-1}}
+            }
+        });
+
+        if(!application){
+            return res.status(404).json({
+                message:"No Applications",
+                success: false
+            })
+        }
+
+        return res.status(200).json({
+            application,
+            success: true
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+//check admin how many applicants can applied
+export const getApplicants = async (req,res) => {
+    try {
+        const jobId = req.params.id;
+        const job = await Job.findById(jobId).populate({
+            path: 'applications',
+            options:{sort:{createdAt:-1}},
+            populate:{
+                path:'applicant' //nested populate
+            }
+        });
+
+        if(!job){
+            return res.status(404).json({
+                message: "Job not found.",
+                success: false
+            })
+        }
+
+        return res.status(200).json({
+            job,
+            success: true
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+//update status of applied application
+export const updateStatus = async (req,res) =>{
+    try {
+        const {status} = req.body;
+        const applicationId = req.params.id;
+
+        if(!status){
+            return res.status(400).json({
+                message: "status is required.",
+                success: false
+            })
+        }
+
+        //find the application by application id
+        const application = await Application.findOne({_id:applicationId});
+        if(!application){
+            return res.status(404).json({
+                message:"Application not found.",
+                success:false
+            })
+        }
+
+        //update the status 
+        application.status = status.toLowerCase();
+        await application.save();
+
+        return res.status(200).json({
+            message:"Status updated successfully",
+            status: true
+        })
+    } catch (error) {
+        console.log(error)
     }
 }
---
+++ backend/routes/application.route.js
@@ -0,0 +1,3 @@
+import express from "express";
+import isAuthenticated from "../middlewares/isAuthenticated.js"; //middlewares for update user
+
